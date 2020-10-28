014a0dc85200af1b42fec363fe77e2936ac22c8f
Dimitris C <d.chatzieleftheriou@gmail.com>
2020-10-28 10:04:34 +0000

    Adds support for SwiftPM

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "TPCircularBuffer",
+    products: [
+        .library(name: "TPCircularBuffer", targets: ["TPCircularBuffer"])
+    ],
+    targets: [
+        .target(name: "TPCircularBuffer", dependencies: [])
+    ]
+)
+
